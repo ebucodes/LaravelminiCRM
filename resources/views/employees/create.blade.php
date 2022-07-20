86470ddfd3b5399478d2865913b26c1f02fb8f4d
ebucodes <ebukaohaji@gmail.com>
2022-07-20 11:37:53 +0100

    create

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -5,7 +5,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="#" class="brand-link">
-            <img src="AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="../AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8" style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
 
@@ -14,7 +14,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                <img src="avatar.png" class="img-circle elevation-2" alt="User Image">
+                <img src="../avatar.png" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                 <a href="#" class="d-block">Naruto Uzumaki</a>
