dc06e28b2b867d21d890a4e93e2a1f27106c8fc8
ebucodes <ebukaohaji@gmail.com>
2022-07-20 11:38:03 +0100

    show

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -5,7 +5,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="#" class="brand-link">
-            <img src="AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="../AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
 
@@ -14,7 +14,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                <img src="avatar.png" class="img-circle elevation-2" alt="User Image">
+                <img src="../avatar.png" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                 <a href="#" class="d-block">Naruto Uzumaki</a>
@@ -58,67 +58,67 @@
             </div>
             <!-- /.sidebar -->
         </aside>
-  <main class="content-wrapper">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-header card-title">
-              <strong>Contact Details</strong>
-            </div>           
-            <div class="card-body">
-              <div class="row">
-                <div class="col-md-12">
-                  <div class="form-group row">
-                    <label for="first_name" class="col-md-3 col-form-label">First Name</label>
-                    <div class="col-md-9">
-                      <p class="form-control-plaintext text-muted">{{ $employee->first_name }}</p>
-                    </div>
-                  </div>
+        <main class="content-wrapper">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header card-title">
+                    <strong>Contact Details</strong>
+                  </div>           
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group row">
+                          <label for="first_name" class="col-md-3 col-form-label">First Name</label>
+                          <div class="col-md-9">
+                            <p class="form-control-plaintext text-muted">{{ $employee->first_name }}</p>
+                          </div>
+                        </div>
 
-                  <div class="form-group row">
-                    <label for="last_name" class="col-md-3 col-form-label">Last Name</label>
-                    <div class="col-md-9">
-                      <p class="form-control-plaintext text-muted">{{ $employee->last_name }}</p>
-                    </div>
-                  </div>
+                        <div class="form-group row">
+                          <label for="last_name" class="col-md-3 col-form-label">Last Name</label>
+                          <div class="col-md-9">
+                            <p class="form-control-plaintext text-muted">{{ $employee->last_name }}</p>
+                          </div>
+                        </div>
 
-                  <div class="form-group row">
-                    <label for="email" class="col-md-3 col-form-label">Email</label>
-                    <div class="col-md-9">
-                      <p class="form-control-plaintext text-muted">{{ $employee->email }}</p>
-                    </div>
-                  </div>
+                        <div class="form-group row">
+                          <label for="email" class="col-md-3 col-form-label">Email</label>
+                          <div class="col-md-9">
+                            <p class="form-control-plaintext text-muted">{{ $employee->email }}</p>
+                          </div>
+                        </div>
 
-                  <div class="form-group row">
-                    <label for="phone" class="col-md-3 col-form-label">Phone</label>
-                    <div class="col-md-9">
-                      <p class="form-control-plaintext text-muted">{{ $employee->phone }}</p>
-                    </div>
-                  </div>
+                        <div class="form-group row">
+                          <label for="phone" class="col-md-3 col-form-label">Phone</label>
+                          <div class="col-md-9">
+                            <p class="form-control-plaintext text-muted">{{ $employee->phone }}</p>
+                          </div>
+                        </div>
 
-                  <div class="form-group row">
-                    <label for="company_id" class="col-md-3 col-form-label">Company</label>
-                    <div class="col-md-9">
-                      <p class="form-control-plaintext text-muted">{{ $employee->company->name }}</p>
+                        <div class="form-group row">
+                          <label for="company_id" class="col-md-3 col-form-label">Company</label>
+                          <div class="col-md-9">
+                            <p class="form-control-plaintext text-muted">{{ $employee->company->name }}</p>
+                          </div>
+                        </div>
+                        <hr>
+                        <div class="form-group row mb-0">
+                          <div class="col-md-9 offset-md-3">
+                              {{-- <a href="{{ route('employees.edit', $employee->id)}}" class="btn btn-info">Edit</a>
+                              <a href="{{ route('employees.destroy', $employee->id) }}" class="btn-delete btn btn-outline-danger">Delete</a> --}}
+                              <a href="{{ route('employees.index')}}" class="btn btn-secondary">Go Back</a>
+                            @include('layouts._delete-form')
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <hr>
-                  <div class="form-group row mb-0">
-                    <div class="col-md-9 offset-md-3">
-                        {{-- <a href="{{ route('employees.edit', $employee->id)}}" class="btn btn-info">Edit</a>
-                        <a href="{{ route('employees.destroy', $employee->id) }}" class="btn-delete btn btn-outline-danger">Delete</a> --}}
-                        <a href="{{ route('employees.index')}}" class="btn btn-secondary">Go Back</a>
-                      @include('layouts._delete-form')
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  </main>
+        </main>
 
   @endsection
 
